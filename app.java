e4b485ecf1b3e861fe6fddf81743b1d52bd12034
Sadhika-Rampelli <sadhikarampelli@gmail.com>
2025-12-10 10:37:38 +0530

    Add ForLoopExample class with counting loop
    Added ForLoopExample class with a loop printing counts.

--- app.java
+++ app.java
@@ -4,4 +4,13 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+//developer 2 addeded code*/
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // Loop from i = 1 to i = 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}
 
